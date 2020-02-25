099530650dedaa8770d8a6b571dbe3149d48c7bf
Vaibhav Goel <vaigoel@publicisgroupe.net>
2020-02-25 16:32:06 +0530

    First check in

---
+++ src/test/java/apitesting/SendGetRequest.java
@@ -0,0 +1,14 @@
+package apitesting;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Some of the basic steps in Rest Assured authentication are
+ *  Doing Authentication- using some headers
+ *  Send Request->
+ *  Receive response-> perform validations
+ *
+ */
+public class SendGetRequest {
+}
---
+++ src/test/java/apitesting/SendPostRequest.java
@@ -0,0 +1,4 @@
+package apitesting;
+
+public class SendPostRequest {
+}
